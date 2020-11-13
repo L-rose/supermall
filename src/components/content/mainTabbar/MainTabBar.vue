d444eb41e80de21d4c17524d9bcd22f2199328c9
l-rose <mf20320179@smail.nju.edu.cn>
2020-11-13 21:37:11 +0800

    second

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="red">
+        <img slot="item-corn" src="~assets/img/tabbar/home.png" alt="">
+        <img slot="item-corn-active" src="~assets/img/tabbar/home_active.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="red">
+        <img slot="item-corn" src="~assets/img/tabbar/category.png" alt="">
+        <img slot="item-corn-active" src="~assets/img/tabbar/category_active.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="red">
+        <img slot="item-corn" src="~assets/img/tabbar/cart.png" alt="">
+        <img slot="item-corn-active" src="~assets/img/tabbar/cart_active.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="red">
+        <img slot="item-corn" src="~assets/img/tabbar/profile.png" alt="">
+        <img slot="item-corn-active" src="~assets/img/tabbar/profile_active.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "components/common/tabber/TabBar";
+import TabBarItem from "components/common/tabber/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
